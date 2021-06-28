925f12ea9f3505e41df5cc2069f7c3626adf34b6
Andres-ibanez <cristian.ibanez@aluno.unila.edu.br>
2021-06-28 00:30:04 -0400

    create button storie

---
+++ src/stories/Button.stories.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { Button } from '../components/Button';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+} as Meta;
+
+const Template: Story = (args) => <Button {...args}>Button Teste</Button>;
+
+export const Default = Template.bind({});
+
+Default.args = {
+    disabled: false,
+};
